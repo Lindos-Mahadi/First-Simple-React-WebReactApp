b0dcf6043a8110027ec629760e33fc4782c8c8d9
Lindos-Mahadi <lindosmahadi@.com>
2021-08-11 21:11:43 +0600

    Route SetUp

--- first-simple-reactapp/src/App.js
+++ first-simple-reactapp/src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+import Home from "./components/Home"
+import RestaurantUpdate from "./components/RestaurantUpdate"
+import RestaurantCreate from "./components/RestaurantCreate"
+import RestaurantDetail from "./components/RestaurantDetail"
+import RestaurantSearch from "./components/RestaurantSearch"
+import RestauranstList from "./components/RestaurantList"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/list">List</Link></li>
+          <li><Link to="/create">Create</Link></li>
+          <li><Link to="/search">Search</Link></li>
+          <li><Link to="/details">Details</Link></li>
+          <li><Link to="/update">Update</Link></li>
+        </ul>
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/list">
+          <RestauranstList />
+        </Route>
+        <Route path="/create">
+          <RestaurantCreate />
+        </Route>
+        <Route path="/search">
+          <RestaurantSearch />
+        </Route>
+        <Route path="/details">
+          <RestaurantDetail />
+        </Route>
+        <Route path="/update">
+          <RestaurantUpdate />
+        </Route>
+
+      </Router>
     </div>
   );
 }
