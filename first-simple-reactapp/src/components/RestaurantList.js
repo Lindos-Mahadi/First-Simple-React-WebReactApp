30dcc07119ad38504566f93b4f337a37da27305d
Lindos-Mahadi <lindosmahadi@.com>
2021-08-11 22:18:53 +0600

    API Call in RestaurantListPage

--- first-simple-reactapp/src/components/RestaurantList.js
+++ first-simple-reactapp/src/components/RestaurantList.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 
 class RestaurantList extends Component {
+    constructor() {
+        super();
+        this.state ={
+            list: null,
+        }
+    }
+
+    componentDidMount(){
+        fetch("http://localhost:3000/resList").then((response) =>{
+            response.json().then((result)=>{
+                // console.log(result);
+                this.setState({list:result})
+            })
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>RestaurantList</h1>
+                {
+                    this.state.list?
+                    <div>
+                        {
+                            this.state.list.map((item,i)=>
+                            <div className="list-wrapper">
+                                <span>{item.name}</span>
+                                <span>{item.email}</span>
+                                <span>{item.rating}</span>
+                                <span>{item.address}</span>
+
+                            </div>)
+                        }
+                    </div>
+                    :<p>Please Wait...</p>
+                }
             </div>
         );
     }
